70d4ce4357ab401f2f5d5ed4fc900d5fa0f2df55
Kunal Chauhan <kunalchauhanlion@gmail.com>
2023-08-06 03:54:00 +0530

    Made all the declaration and added all the imports

--- src/src/GamePannel.java
+++ src/src/GamePannel.java
@@ -1,14 +1,59 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
 
 public class GamePannel extends JPanel implements ActionListener {
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNIT = (SCREEN_HEIGHT*SCREEN_WIDTH)/UNIT_SIZE;
+    static final int DELAY = 75;
+
+     final int[] x = new int[GAME_UNIT];
+     final int[] y = new int[GAME_UNIT];
+     int bodyParts = 6;
+     int applesEaten;
+     int appleX;
+     int appleY;
+     char direction = 'R';
+     boolean running = false;
+     Timer timer;
+     Random random;
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
     }
-
     GamePannel(){
 
+    }
+    public void startGame(){
+
+    }
+    public void paintComponent(Graphics g){
+
+    }
+    public void draw(Graphics g){
+
+    }
+    public void move(){
+
+    }
+    public void checkApple(){
+
+    }
+    public void checkCollision(){
+
+    }
+    public void gameOver(Graphics g){
+
+    }
+    public class MyKepAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e){
+
+        }
     }
 }
