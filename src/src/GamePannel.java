165cbe906c4afe3f3e8261dcfa00503edaa61e8a
Kunal Chauhan <kunalchauhanlion@gmail.com>
2023-08-06 18:56:14 +0530

    Added some art stuff makes it look cool uk
    😎✅  ☆*: .｡. o(≧▽≦)o .｡.:*☆

--- src/src/GamePannel.java
+++ src/src/GamePannel.java
@@ -51,10 +51,10 @@ public class GamePannel extends JPanel implements ActionListener {
     }
     public void draw(Graphics g){
         if (running){
-            for (int i = 0 ; i < SCREEN_HEIGHT / UNIT_SIZE ; i++){
-                g.drawLine(i * UNIT_SIZE , 0 , i * UNIT_SIZE , SCREEN_HEIGHT);
-                g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i * UNIT_SIZE);
-            }
+//            for (int i = 0 ; i < SCREEN_HEIGHT / UNIT_SIZE ; i++){
+//                g.drawLine(i * UNIT_SIZE , 0 , i * UNIT_SIZE , SCREEN_HEIGHT);
+//                g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i * UNIT_SIZE);
+//            }
             g.setColor(Color.RED);
             g.fillOval(appleX,appleY,UNIT_SIZE,UNIT_SIZE);
 
@@ -65,6 +65,7 @@ public class GamePannel extends JPanel implements ActionListener {
                 }
                 else{
                     g.setColor(new Color(45,180,0));
+                    g.setColor(new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255)));
                     g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
                 }
             }
